9e1961f8838d7a718150897b29a3f6f756e08f51
David Z <62783374+DavidZhou79@users.noreply.github.com>
2021-06-26 17:41:32 -0400

    Update main.js

--- testeditor/main.js
+++ testeditor/main.js
@@ -9,6 +9,11 @@ function createWindow () {
       win = new BrowserWindow({
       width: 800,
       height: 500,
+      maxWidth: 800,
+      maxHeight: 500,
+      minWidth: 800,
+      minHeight: 500,
+      icon: 'icon.ico',
       webPreferences: {
           nodeIntegration: true,
           contextIsolation: false,
